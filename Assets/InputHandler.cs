3b1cffa7324c96d64a82cd1c57f66aa9bc09046e
Andreas Berglund <andreas.berglund@acino.se>
2017-04-06 20:59:34 +0200

    Initial commit of TimeManagement

---
+++ Assets/InputHandler.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour {
+
+	[SerializeField] Sprite sprite1;
+	[SerializeField] Sprite sprite2;
+	[SerializeField] Sprite sprite3;
+	[SerializeField] Sprite sprite4;
+
+	[SerializeField] SpriteRenderer[] sprites;
+
+	private int nextInput;
+
+	// Use this for initialization
+	void Start() {
+		nextInput = 0;
+	}
+
+	// Update is called once per frame
+	void Update() {
+		if (nextInput >= sprites.Length) {
+			nextInput = 0;
+			foreach (var sprite in sprites) {
+				sprite.sprite = null;
+			}
+		}
+
+		if (Input.GetKeyDown(KeyCode.A)) {
+			sprites[nextInput].sprite = sprite1;
+			nextInput++;
+		}
+		if (Input.GetKeyDown(KeyCode.S)) {
+			sprites[nextInput].sprite = sprite2;
+			nextInput++;
+		}
+		if (Input.GetKeyDown(KeyCode.D)) {
+			sprites[nextInput].sprite = sprite3;
+			nextInput++;
+		}
+		if (Input.GetKeyDown(KeyCode.F)) {
+			sprites[nextInput].sprite = sprite4;
+			nextInput++;
+		}
+	}
+}
---
+++ Assets/W.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class W : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.E)) {
+			Debug.Log("Du tryckte på E");
+		}
+		if (Input.GetKeyDown (KeyCode.W)) {
+			Debug.Log("Du tryckte på W");
+		}
+		if (Input.GetKeyDown (KeyCode.A)) {
+			Debug.Log("Du tryckte på A");
+		}
+		if (Input.GetKeyDown (KeyCode.S)) {
+			Debug.Log("Du tryckte på S");
+		}
+	}
+}
